d09874ff0779555ae5e701655e9081d3580ae683
Alex Goi <alexmoraisgoi@gmail.com>
2024-09-03 20:24:41 -0300

    tentativa de commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
@@ -46,6 +47,106 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("You care"),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Container(
+                width: MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.all(27),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 30,),
+                    const Text("Digite seus dados aqui"),
+                    const SizedBox(height: 10,),
+                    const CupertinoTextField(
+                      cursorColor: Colors.black,
+                      padding: EdgeInsets.all(15),
+                      placeholder: "Digite seu email",
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(7))
+                      ),
+                    ),
+                    const SizedBox(height: 15),
+                    const CupertinoTextField(
+                      cursorColor: Colors.black,
+                      padding: EdgeInsets.all(15),
+                      placeholder: "Digite sua senha",
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(7))
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      width: double.infinity,
+                      child: CupertinoButton(
+                        padding: const EdgeInsets.all(17), 
+                        color: Colors.greenAccent,
+                        child: const Text(
+                          "Acessar",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        onPressed: () {},
+                      ),
+                      ),
+                      const SizedBox(height: 7),
+                      Container(
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.greenAccent,) 
+                        ),
+                        child: CupertinoButton(
+                          child: const Text(
+                            "Crie a sua conta",
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontWeight: FontWeight.w600,
+                            ),
+                        ),
+                         onPressed: (){
+                          showDialog(context: context,
+                           barrierDismissible: true, 
+                           builder: (BuildContext cxt){
+                            return const AlertDialog(
+                              content: CriarConta(),
+                            );
+                           } 
+                           );
+                         }),
+                      )
+                    ],
+                  ),
+                ),
+            
+        );
+  }
+}
+
+class CriarConta extends StatefulWidget {
+  const CriarConta({super.key});
+
+  @override
+  State<CriarConta> createState() => _NestedDialogState();
+}
+
+class _NestedDialogState extends State<CriarConta> {
+  @override
+  Widget build(BuildContext context) {
+    return const AlertDialog(
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: [
+            Text("Você clicou em criar sua conta!")
+          ],
+        ),
+      ),
+    );
   }
 }
